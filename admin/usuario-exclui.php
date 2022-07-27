c43482911a8aff1bc78f8825db18324775c179bf
msakal <m_sakalauskas@hotmail.com>
2022-07-27 08:40:04 -0300

    atualizando o método excluir

--- admin/usuario-exclui.php
+++ admin/usuario-exclui.php
@@ -1,14 +1,18 @@
 <?php
 
-use Microblog\Usuario;
+    use Microblog\Usuario;
+    require_once "../vendor/autoload.php";
 
-    require_once "../inc/cabecalho-admin.php";
+    // Criamos um obj para poder acessar os recursos da Classe
+    $OBJusuario = new Usuario;  // Não esqueça do autoload e do namespace
 
-    $OBJusuario = new Usuario;
-
+    // Obtemos o id da URL eo o passamos para o setter
     $OBJusuario->setId($_GET['id']);
+
+    // Só então executamos o método de exclusão
     $OBJusuario->excluir();
 
+    // Após excluir, redirecionamento a página de lista de usuários
     header("location:usuarios.php");
 
 ?>
